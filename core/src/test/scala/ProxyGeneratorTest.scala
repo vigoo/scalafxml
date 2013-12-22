3811ab9afab2980cfdb739889b5a75f5c63059a9
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2013-12-22 17:55:03 +0100

    Removed obsolete test cases

--- core/src/test/scala/ProxyGeneratorTest.scala
+++ core/src/test/scala/ProxyGeneratorTest.scala
@@ -7,20 +7,4 @@ import FxmlProxyGenerator._
 
 class ProxyGeneratorTest extends FlatSpec with Matchers {
 
-  "toJavaFXType" should " return its input if it is not a ScalaFX type" in {
-    
-    toJavaFXTypeOrOriginal(typeOf[String]) should be (typeOf[String])
-    toJavaFXTypeOrOriginal(typeOf[List[String]]) should be (typeOf[List[String]])
-    toJavaFXTypeOrOriginal(typeOf[org.scalatest.FlatSpec]) should be (typeOf[org.scalatest.FlatSpec])
-  }
-  
-  it should " convert ScalaFX controls to JavaFX controls" in {
-    
-    assert(toJavaFXTypeOrOriginal(typeOf[scalafx.scene.control.Button]) =:= typeOf[javafx.scene.control.Button])
-    assert(toJavaFXTypeOrOriginal(typeOf[scalafx.scene.control.TextField]) =:= typeOf[javafx.scene.control.TextField])
-    assert(toJavaFXTypeOrOriginal(typeOf[scalafx.scene.control.ListView[String]]) =:= typeOf[javafx.scene.control.ListView[String]])
-  }
-  
-
-  // TODO: test generated proxies
 }
